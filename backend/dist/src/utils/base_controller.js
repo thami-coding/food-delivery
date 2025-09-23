25246e2692db54fed4ada2584137e5ab297d44cb
Thami <sainttsquared@gmail.com>
2025-09-23 20:31:40 +0200

    remove dist and node_modules folder

--- backend/dist/src/utils/base_controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseController = void 0;
-class BaseController {
-}
-exports.BaseController = BaseController;
--- backend/dist/src/utils/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_CONST = void 0;
-exports.SERVER_CONST = {
-    JWTSECRET: "498dc42f5394bb2c948ae7f9ab9ddc8d",
-    ACCESS_TOKEN_EXPIRY_TIME_SECONDS: 1 * 8 * 60 * 60,
-    REFRESH_TOKEN_EXPIRY_TIME_SECONDS: 5 * 7 * 24 * 60 * 60,
-};
