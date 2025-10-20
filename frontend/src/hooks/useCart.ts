6c90bc47d45450567480c24ef0281a5562116edc
Thami <sainttsquared@gmail.com>
2025-10-20 19:43:09 +0200

    (feature) add cart hook

--- frontend/src/hooks/useCart.ts
+++ frontend/src/hooks/useCart.ts
@@ -1,9 +1,26 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchCart } from "../lib/apiClient";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchCurrentUsersCart, updateQuantity } from "../lib/apiClient";
 
 export const useCart = () => {
-  return useQuery({
-    queryKey: ["cart"],
-    queryFn: () => fetchCart(),
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["currentUsersCart"],
+    queryFn: () => fetchCurrentUsersCart(),
+  });
+
+  return { data, isPending, isError, error };
+};
+
+
+export const useUpdateCart = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateQuantity,
+    onSuccess: (userData) => {
+      queryClient.setQueryData(["currentUsersCart"], userData);
+    },
+    onError: (error) => {
+      console.error("cart update failed:", error);
+    },
   });
 };
