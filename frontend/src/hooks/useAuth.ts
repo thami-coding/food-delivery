a120c8959d7de0990c50ec1b174ca405b62ba4af
Thami <sainttsquared@gmail.com>
2025-10-20 19:43:48 +0200

    add auth hook for authentication

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,11 +1,65 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchUser } from "../lib/apiClient";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchUser, login, logout, signUp } from "../lib/apiClient";
+import { useNavigate } from "react-router";
 
 export const useUser = () => {
-  return useQuery({
-    queryKey: ["user"],
+  const {
+    data: user,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["currentUser"],
     queryFn: fetchUser,
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
     retry: false,
-    staleTime: 5 * 60 * 1000,
+  });
+
+  return { user, isPending, isError, error };
+};
+
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: login,
+    onSuccess: (userData) => {
+      queryClient.setQueryData(["currentUser"], userData);
+      navigate("/cart");
+    },
+    onError: (error) => {
+      console.error("Login failed:", error);
+    },
+  });
+};
+
+export const useSignup = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: signUp,
+    onSuccess: (userData) => {
+      queryClient.setQueryData(["currentUser"], userData);
+      navigate("/cart");
+    },
+    onError: (error) => {
+      console.error("Login failed:", error);
+    },
+  });
+};
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      queryClient.setQueryData(["currentUser"], null);
+      navigate("/login");
+    },
   });
 };
