f6895dec32aab12664eb00818b9f82c0cefb4197
Thami <sainttsquared@gmail.com>
2025-10-20 19:54:51 +0200

    (refactor) remove globalState hook

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,113 +1,137 @@
 import { Link, useNavigate } from "react-router";
 import logo from "../assets/logo.png";
-import { fetchCart, login, signUp } from "../lib/apiClient";
 import { useState } from "react";
-import { useGlobalState } from "../hooks/useGlobalState";
+import FormInput from "./FormInput";
+import * as z from "zod";
+import { useLogin, useSignup } from "../hooks/useAuth";
 
 const LoginForm = ({ pathname }: { pathname: string }) => {
-  const [username, setUsername] = useState("second");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+  const { mutate: login, isPending: isLoginPending } = useLogin();
+  const { mutate: signup, isPending: isSignupPending } = useSignup();
 
-  const { dispatch } = useGlobalState();
+  const [error, setError] = useState({
+    emailError: "",
+    passwordError: "",
+  });
 
   const isSignup = pathname === "/signup";
   const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try {
-      setLoading(true);
-      if (!username || !password || !email) {
-        throw new Error("Some fields are missing");
-      }
+    let emailErrorMessage = "";
+    let passwordErrorMessage = "";
+    const emailValidation = z.string().email();
+    const passwordValidation = z
+      .string({ message: "Password field can not be empty" })
+      .min(5, { message: "Password must be at least 5 characters long." });
 
-      if (isSignup) {
-        await signUp(username, email, password);
-        navigate("/login");
+    const passwordResult = passwordValidation.safeParse(password);
+    const emailResult = emailValidation.safeParse(email);
+
+    emailErrorMessage = emailResult.error?.issues[0].message || "";
+    passwordErrorMessage = passwordResult.error?.issues[0].message || "";
+
+    if (emailErrorMessage || passwordErrorMessage) {
+      setError({
+        emailError: emailErrorMessage,
+        passwordError: passwordErrorMessage,
+      });
+      return;
+    }
+
+    if (isSignup) {
+      const passwordsMatch = z
+        .object({
+          password: z
+            .string({
+              message: "Password must be at least 5 characters long.",
+            })
+            .min(5, {
+              message: "Password must be at least 5 characters long.",
+            }),
+          confirm: z
+            .string({
+              message: "Password must be at least 5 characters long.",
+            })
+            .min(5, {
+              message: "Password must be at least 5 characters long.",
+            }),
+        })
+        .refine((data) => data.password === data.confirm, {
+          message: "Passwords don't match",
+          path: ["password"],
+        });
+      const passwordMatchResult = passwordsMatch.safeParse({
+        password,
+        confirm: confirmPassword,
+      });
+      passwordErrorMessage = passwordMatchResult.error?.issues[0].message || "";
+      if (passwordErrorMessage) {
+        setError({
+          emailError: passwordErrorMessage,
+          passwordError: emailErrorMessage,
+        });
         return;
       }
-      const { data: user } = await login(email, password);
-      const cart = await fetchCart();
-      dispatch({ type: "LOGIN", payload: user });
-      dispatch({ type: "SET_CART", payload: cart });
-      navigate("/");
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
+
+      const username = email.split("@")[0];
+      signup({ username, email, password });
+      setError({ passwordError: "", emailError: "" });
+      navigate("/login");
+      return;
     }
-    
+
+    login({ email, password });
+    setError({ passwordError: "", emailError: "" });
   };
 
   return (
-    <article className="fixed top-[3rem] left-2/5 border border-white rounded-md text-white p-8 w-90 bg-[#202020] ">
+    <article className="fixed top-[3rem] left-2/5 border border-white rounded-md text-white p-8 w-[28rem] bg-[#202020] ">
       <div className="border w-fit mr-auto ml-auto mb-9">
         <img src={logo} alt="" />
       </div>
       <form onSubmit={handleSubmit}>
-        {isSignup && (
-          <div className="flex flex-col mb-6">
-            <label htmlFor="name">Name*</label>
-            <input
-              type="text"
-              className="border rounded-md mt-2  py-1.5 pl-3 text-gray-300  focus:border-yellow-400 focus:outline focus:outline-yellow-400"
-              placeholder="Enter your username"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          errorMessage={error.emailError}
+          setValue={setEmail}
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          errorMessage={error.passwordError}
+          setValue={setPassword}
+        />
+        {!isSignup && (
+          <Link
+            to="/reset-password"
+            className="text-xs block -mt-4 text-left mb-10 hover:underline"
+          >
+            forgotten?
+          </Link>
         )}
 
-        <div className="flex flex-col mb-6">
-          <label htmlFor="name">Email*</label>
-          <input
-            type="email"
-            className="border rounded-md mt-2  py-1.5 pl-3 text-gray-300  focus:border-yellow-400 focus:outline focus:outline-yellow-400"
-            placeholder="Enter your email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-col mb-6">
-          <label htmlFor="name">Password*</label>
-          <input
-            type="password"
-            className="border rounded-md mt-2  py-1.5 pl-3 text-gray-300  focus:border-yellow-400 focus:outline focus:outline-yellow-400"
-            placeholder="Enter password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {!isSignup && (
-            <Link
-              to="/reset-password"
-              className="text-end text-xs mt-1 hover:underline"
-            >
-              forgotten?
-            </Link>
-          )}
-        </div>
         {isSignup && (
-          <div className="flex flex-col mb-6">
-            <label htmlFor="confirmPassword">Confirm Password*</label>
-            <input
-              type="password"
-              className="border rounded-md mt-2  py-1.5 pl-3 text-gray-300  focus:border-yellow-400 focus:outline focus:outline-yellow-400"
-              placeholder="Confirm password"
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
+          <FormInput
+            type="password"
+            name="confirm password"
+            value={confirmPassword}
+            errorMessage=""
+            setValue={setConfirmPassword}
+          />
         )}
 
         <button
-          disabled={loading}
-          className="bg-amber-300 w-full text-black p-1.5 rounded-md cursor-pointer hover:bg-amber-400"
+          disabled={isLoginPending || isSignupPending}
+          className={`w-full text-black p-1.5 rounded-md cursor-pointer hover:bg-amber-400 ${
+            isLoginPending || isSignupPending ? "bg-gray-500" : "bg-amber-300"
+          }`}
         >
           {isSignup ? "Create account" : "Login"}
         </button>
@@ -115,14 +139,14 @@ const LoginForm = ({ pathname }: { pathname: string }) => {
       <p className="text-xs text-center mt-3">
         {isSignup ? (
           <>
-            Already have an account?{" "}
+            Already have an account?{"  "}
             <Link to="/login" className="text-yellow-400 underline">
               Login
             </Link>
           </>
         ) : (
           <>
-            Don't have an account?{" "}
+            Don't have an account ?{" "}
             <Link to="/signup" className="text-yellow-400 underline">
               Signup
             </Link>
