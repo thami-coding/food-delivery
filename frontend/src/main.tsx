a9308acb24a9a806f4eb1bf9e7446c2a20ee119d
Thami <sainttsquared@gmail.com>
2025-10-20 19:35:31 +0200

    (refactor) remove global context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { GlobalStateProvider } from "./context/GlobalStateProvider.tsx";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <GlobalStateProvider>
-        <App />
-      </GlobalStateProvider>
+      <App />
     </QueryClientProvider>
   </StrictMode>
 );
