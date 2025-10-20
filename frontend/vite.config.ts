c22c74d20115b800be3d43eae32a8d8e4d5fe0bf
Thami <sainttsquared@gmail.com>
2025-10-20 19:22:47 +0200

    change server proxy for docker connections

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://backend:5000", // for dokcer
+        // target: "http://localhost:5000", //for local
         changeOrigin: true,
         secure: false,
       },
