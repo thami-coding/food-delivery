ed4b564946f40491254f9b826894930f0cb76c1e
Thami <sainttsquared@gmail.com>
2025-10-20 19:41:30 +0200

    remove hook redundant

--- frontend/src/hooks/useGlobalState.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { GlobalContext } from "../context/GlobalStateContext";
-
-export const useGlobalState = () => {
-  const context = useContext(GlobalContext);
-  if (!context)
-    throw new Error("useGlobalState must be used within GlobalStateProvider");
-  return context;
-};
