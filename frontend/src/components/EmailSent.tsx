0cbe4a239b2817802146f06f482c80da79ba0565
Thami <sainttsquared@gmail.com>
2026-01-07 16:21:17 +0200

    add resend functionality

--- frontend/src/components/EmailSent.tsx
+++ frontend/src/components/EmailSent.tsx
@@ -1,8 +1,17 @@
 import { TfiEmail } from "react-icons/tfi";
+import { useForgotPassword } from "../hooks/useAuth";
 
 export default function EmailSent({ email }: { email: string }) {
+ const { mutate, isPending, isError, error } = useForgotPassword()
+ const handleClick = () => {
+  mutate(email)
+ }
+
+ if (isError) {
+  console.log(error);
+ }
  return (
-  <article className="ml-auto mr-auto mt-15 py-15 shadow-lg shadow-neutral-900 rounded-md text-white px-8 w-[28rem] bg-[#202020] ">
+  <article className="ml-auto mr-auto mt-15 py-15 shadow-lg shadow-neutral-900 rounded-md text-white px-12 w-[28rem] bg-[#202020] ">
    <div className="border-2 border-green-600 text-green-600 rounded-full text-5xl p-6 w-fit mr-auto ml-auto mb-6 block">
     <TfiEmail />
    </div>
@@ -10,9 +19,12 @@ export default function EmailSent({ email }: { email: string }) {
    <p className='mb-3 text-center text-neutral-300 px-10'>Please check the email address {email} for instructions to reset your password.</p>
    <div className='mt-8'>
     <button
-     className="w-full text-neutral-900 text-[1.2rem] py-3 text- rounded-md cursor-pointer hover:bg-amber-400 bg-amber-300"
+     onClick={handleClick}
+     disabled={isPending}
+     className="w-full text-neutral-900 text-[1.2rem] py-3 rounded-md cursor-pointer hover:bg-amber-400 bg-amber-300"
     >
-     Resend email
+     {isPending ? "Loading..."
+      : "Resend email"}
     </button>
    </div>
   </article>
