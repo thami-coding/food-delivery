da8e4d3ced7c941933c5cb202081b178e307b1c8
Thami <thamig125@gmail.com>
2026-02-11 14:44:28 +0200

    refactor imports

--- frontend/src/features/user/components/EmailSent.tsx
+++ frontend/src/features/user/components/EmailSent.tsx
@@ -1,32 +1,35 @@
-import { TfiEmail } from "react-icons/tfi";
-import { useForgotPassword } from "../hooks/useAuth";
+import { TfiEmail } from "react-icons/tfi"
+import { useForgotPassword } from "../../auth/hooks"
+
 
 export default function EmailSent({ email }: { email: string }) {
- const { mutate, isPending, isError, error } = useForgotPassword()
- const handleClick = () => {
-  mutate(email)
- }
+  const { mutate, isPending, isError, error } = useForgotPassword()
+  const handleClick = () => {
+    mutate(email)
+  }
 
- if (isError) {
-  console.log(error);
- }
- return (
-  <article className="ml-auto mr-auto mt-15 py-15 shadow-lg shadow-neutral-900 rounded-md text-white px-12 w-[28rem] bg-[#202020] ">
-   <div className="border-2 border-green-600 text-green-600 rounded-full text-5xl p-6 w-fit mr-auto ml-auto mb-6 block">
-    <TfiEmail />
-   </div>
-   <h3 className='text-center text-2xl mb-6'>Check your email</h3>
-   <p className='mb-3 text-center text-neutral-300 px-10'>Please check the email address {email} for instructions to reset your password.</p>
-   <div className='mt-8'>
-    <button
-     onClick={handleClick}
-     disabled={isPending}
-     className="w-full text-neutral-900 text-[1.2rem] py-3 rounded-md cursor-pointer hover:bg-amber-400 bg-amber-300"
-    >
-     {isPending ? "Loading..."
-      : "Resend email"}
-    </button>
-   </div>
-  </article>
- )
+  if (isError) {
+    console.log(error)
+  }
+  return (
+    <article className="ml-auto mr-auto mt-15 py-15 shadow-lg shadow-neutral-900 rounded-md text-white px-12 w-[28rem] bg-[#202020] ">
+      <div className="border-2 border-green-600 text-green-600 rounded-full text-5xl p-6 w-fit mr-auto ml-auto mb-6 block">
+        <TfiEmail />
+      </div>
+      <h3 className="text-center text-2xl mb-6">Check your email</h3>
+      <p className="mb-3 text-center text-neutral-300 px-10">
+        Please check the email address {email} for instructions to reset your
+        password.
+      </p>
+      <div className="mt-8">
+        <button
+          onClick={handleClick}
+          disabled={isPending}
+          className="w-full text-neutral-900 text-[1.2rem] py-3 rounded-md cursor-pointer hover:bg-amber-400 bg-amber-300"
+        >
+          {isPending ? "Loading..." : "Resend email"}
+        </button>
+      </div>
+    </article>
+  )
 }
