7afc262bc08a73ad4316e3e62542f9dd3409ddc1
Thami <thamig125@gmail.com>
2026-02-11 14:22:42 +0200

    refactor remove FormInput component

--- frontend/src/pages/ForgotPasswordPage.tsx
+++ frontend/src/pages/ForgotPasswordPage.tsx
@@ -1,11 +1,11 @@
 import { useState, type FormEvent } from "react"
-import FormInput from "../components/FormInput"
 import isEmail from "validator/lib/isEmail"
-import { useForgotPassword } from "../hooks/useAuth"
 import { toast } from "react-toastify"
 import { Link } from "react-router"
 import logo from "../assets/logo.png"
 import EmailSent from "../features/user/components/EmailSent"
+import { useForgotPassword } from "../features/auth/hooks"
+import ErrorMessage from "../components/ErrorMessage"
 
 export default function ForgotPaswordPage() {
   const [email, setEmail] = useState("")
@@ -13,7 +13,6 @@ export default function ForgotPaswordPage() {
   const { mutate, isPending, isSuccess, isError, error, data } =
     useForgotPassword()
 
-  const notify = (error: string | undefined) => toast(error)
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
@@ -25,9 +24,7 @@ export default function ForgotPaswordPage() {
     mutate(email)
   }
 
-  if (isError) {
-    notify(error?.message)
-  }
+
   if (isSuccess) {
     return <EmailSent email={email} />
   }
@@ -37,24 +34,24 @@ export default function ForgotPaswordPage() {
       <Link to="/" className="border w-fit mr-auto ml-auto mb-10 block">
         <img src={logo} alt="" />
       </Link>
-      <h3 className="text-center text-2xl mb-6">Forgot your Password?</h3>
+      {/* <h3 className="text-center text-2xl mb-6">Forgot your Password?</h3> */}
       <p className="mb-3 text-center text-neutral-300">
         Enter your email address and we will send you instructons to reset your
         password.
       </p>
       <form onSubmit={handleSubmit} className="mt-8">
-        <FormInput
-          type="email"
-          name=""
-          value={email}
-          placeholder="Enter email to reset password"
-          errorMessage={emailError}
-          setValue={setEmail}
-        />
+        <div className="flex flex-col relative mb-2.5 h-20">
+          <input
+            className="border rounded-md py-2.5 pl-3 text-gray-300 mt-1 focus:border-yellow-400 focus:outline focus:outline-yellow-400"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <ErrorMessage message={emailError} />
+        </div>
 
         <button
           disabled={isPending}
-          className={`w-full text-neutral-900 text-[1.2rem] py-2 mt-3 rounded-md cursor-pointer hover:bg-amber-400 ${
+          className={`w-full text-neutral-900 text-[1.2rem] py-2 rounded-md cursor-pointer hover:bg-amber-400 ${
             isPending ? "bg-gray-500" : "bg-amber-300"
           }`}
         >
