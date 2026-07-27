5ce56978bdef5ed230b2cfa46667b6166d6d05f6
Thami <thamig125@gmail.com>
2026-07-27 20:48:30 +0200

    fix configuration

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,37 +1,33 @@
-import winston from "winston";
+import winston from "winston"
 
-const { combine, timestamp, errors, json, colorize, printf } = winston.format;
+const { combine, timestamp, errors, json, colorize } = winston.format
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production"
+
+const isVercel = !!process.env.VERCEL
 
 export const logger = winston.createLogger({
- level: isProd ? "info" : "debug",
- format: combine(
-  timestamp(),
-  errors({ stack: true }),
-  json()
- ),
- transports: [
-  new winston.transports.File({
-   filename: "logs/error.log",
-   level: "error",
-  }),
-  new winston.transports.File({
-   filename: "logs/combined.log",
-  }),
- ],
-});
+  level: isProd ? "info" : "debug",
+  format: combine(timestamp(), errors({ stack: true }), json()),
+  transports: [
+    new winston.transports.Console({
+      format: isProd
+        ? json()
+        : combine(colorize({ all: true }), winston.format.simple()),
+    }),
+  ],
+})
 
-// Pretty console logs in development
-if (!isProd) {
- logger.add(
-  new winston.transports.Console({
-   format: combine(
-    colorize({ all: true }),
-    timestamp(),
-    errors({ stack: true }),
-    winston.format.simple()
-   ),
-  })
- );
+if (!isProd && !isVercel) {
+  logger.add(
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+    }),
+  )
+  logger.add(
+    new winston.transports.File({
+      filename: "logs/combined.log",
+    }),
+  )
 }
