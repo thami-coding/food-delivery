430cc4cc5d0e650d2fa24f8497ee434225bd8ddf
Thami <thamig125@gmail.com>
2026-08-27 23:43:54 +0200

    Modify api docs route

--- backend/src/api/v1/routes/index.ts
+++ backend/src/api/v1/routes/index.ts
@@ -7,7 +7,6 @@ import orderRoutes from "./order.routes"
 import docsRouter from "./docs.routes"
 import paymentRouter from "./payment.routes"
 
-
 const router = express.Router()
 
 router.use("/auth", authRoutes)
@@ -16,7 +15,7 @@ router.use("/products", productRoutes)
 router.use("/cart", cartRoutes)
 router.use("/orders", orderRoutes)
 router.use("/payment", paymentRouter)
-router.use("/", docsRouter)
+router.use("/api-docs", docsRouter)
 
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to the V1 API" })
