6c770617db92b859f733f9cfddeef6f56ac4b79b
Thami <thamig125@gmail.com>
2026-09-19 11:10:28 +0200

    Fix bypass Deployment Protection

--- tests-e2e/playwright.config.ts
+++ tests-e2e/playwright.config.ts
@@ -31,7 +31,13 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASE_URL || "http://localhost:5173",
+    baseURL:
+      process.env.BASE_URL ||
+      "https://food-delivery-frontend-git-dev-thamis-projects-6f381ad9.vercel.app",
+    extraHTTPHeaders: {
+      "x-vercel-protection-bypass":
+        process.env.VERCEL_AUTOMATION_BYPASS_SECRET || "",
+    },
     actionTimeout: 15000, // Increase action timeout
     navigationTimeout: 30_000,
 
